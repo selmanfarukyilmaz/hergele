76dac8b4a4b362d235124da1d7dfc76a60b655a6
selman <aksamavisi@gmail.com>
2023-05-01 23:30:38 +0300

    datetime fix

--- DB_operations/card.py
+++ DB_operations/card.py
@@ -24,63 +24,63 @@ def create_card(guid: str, cc_owner: str, cc_number: str, exp_month: str, exp_ye
     :param cc_uniq_id: Card Uniq ID
     :return: response and status code
     """
-    try:
-        collection = db["users"]
-        owner = collection.find_one({"GUID": guid}, {"_id": 1, "userNo": 1})
-        if username != owner["userNo"]:
-            return {"Sonuc": "0", "Sonuc_Str": "Başarısız",
-                    "KS_GUID": ""}, 404
-        card_uuid = str(uuid.uuid4())
-        post = {
-            "GUID": card_uuid,
-            "KK_Sahibi_GUID": guid,
-            "KK_Sahibi": cc_owner,
-            "KK_No": cc_number,
-            "KK_SK_Ay": exp_month,
-            "KK_SK_Yil": exp_year,
-            "balance": float(0),
-            "security_number": "000",
-            "3d_secure_password": "34020",
-            "KK_Banka": "YAPIKREDİ A.Ş.",
-            "KK_tip": "VISA",
-            "KK_Marka": "Maximum",
-            "KK_KD": "Kredi Kartı",
-            "KK_SK": f"{exp_year}-{exp_month}-31T00:00:00+03:00",
-            "KK_Sahibi_id": str(owner['_id']),
-            "create_date": datetime.date.today()}
-        if card_name:
-            post["KK_Kart_Adi"] = card_name
-        if cc_uniq_id:
-            post["KK_Islem_ID"] = cc_uniq_id
+    # try:
+    collection = db["users"]
+    owner = collection.find_one({"GUID": guid}, {"_id": 1, "userNo": 1})
+    if username != owner["userNo"]:
+        return {"Sonuc": "0", "Sonuc_Str": "Başarısız",
+                "KS_GUID": ""}, 404
+    card_uuid = str(uuid.uuid4())
+    post = {
+        "GUID": card_uuid,
+        "KK_Sahibi_GUID": guid,
+        "KK_Sahibi": cc_owner,
+        "KK_No": cc_number,
+        "KK_SK_Ay": exp_month,
+        "KK_SK_Yil": exp_year,
+        "balance": float(0),
+        "security_number": "000",
+        "3d_secure_password": "34020",
+        "KK_Banka": "YAPIKREDİ A.Ş.",
+        "KK_tip": "VISA",
+        "KK_Marka": "Maximum",
+        "KK_KD": "Kredi Kartı",
+        "KK_SK": f"{exp_year}-{exp_month}-31T00:00:00+03:00",
+        "KK_Sahibi_id": str(owner['_id']),
+        "create_date": str(datetime.date.today())}
+    if card_name:
+        post["KK_Kart_Adi"] = card_name
+    if cc_uniq_id:
+        post["KK_Islem_ID"] = cc_uniq_id
 
-        collection = db["cards"]
-        collection.insert_one(post)
+    collection = db["cards"]
+    collection.insert_one(post)
 
-        collection = db["users"]
-        collection.find_one_and_update({'GUID': guid}, {'$push': {'allCards': f"{card_uuid}"}})
+    collection = db["users"]
+    collection.find_one_and_update({'GUID': guid}, {'$push': {'allCards': f"{card_uuid}"}})
 
-        response = {"Sonuc": 1,
-                    "Sonuc_Str": "Başarılı",
-                    "KS_GUID": str(card_uuid)}
+    response = {"Sonuc": 1,
+                "Sonuc_Str": "Başarılı",
+                "KS_GUID": str(card_uuid)}
 
-        # collection = db["responses"]
-        #
-        # collection.insert_one({"Sonuc": 1,
-        #                        "Sonuc_Str": "Başarılı",
-        #                        "KS_GUID": str(card_uuid),
-        #                        "status_code": 201,
-        #                        "method": "KS_Kart_Ekle"})
+    # collection = db["responses"]
+    #
+    # collection.insert_one({"Sonuc": 1,
+    #                        "Sonuc_Str": "Başarılı",
+    #                        "KS_GUID": str(card_uuid),
+    #                        "status_code": 201,
+    #                        "method": "KS_Kart_Ekle"})
 
-        return response, 201
-    except Exception as a:
-        response = {"Sonuc": 0,
-                    "Sonuc_Str": "Başarısız - Bilinmeyen Hata",
-                    "KS_GUID": "",
-                    "status_code": 400,
-                    "method": "KS_Kart_Ekle"}
-        # collection = db["responses"]
-        # collection.insert_one(response)
-        return response, 400
+    return response, 201
+    # except Exception as a:
+    #     response = {"Sonuc": 0,
+    #                 "Sonuc_Str": "Başarısız - Bilinmeyen Hata",
+    #                 "KS_GUID": "",
+    #                 "status_code": 400,
+    #                 "method": "KS_Kart_Ekle"}
+    #     # collection = db["responses"]
+    #     # collection.insert_one(response)
+    #     return response, 400
 
 
 @check_user_exist
